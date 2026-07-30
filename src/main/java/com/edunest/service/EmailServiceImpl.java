0c803c1d08e8293bbd7f337ed1b300ce237b3d48
Harshid-Patel <averisitservice@gmail.com>
2026-07-30 16:14:59 +0530

    added html

--- src/main/java/com/edunest/service/EmailServiceImpl.java
+++ src/main/java/com/edunest/service/EmailServiceImpl.java
@@ -6,10 +6,13 @@ import jakarta.mail.internet.MimeMessage;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StreamUtils;
 
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 @Slf4j
@@ -22,13 +25,18 @@ public class EmailServiceImpl implements EmailService {
     @Value("${spring.mail.username}")
     private String fromEmail;
 
-    private void sendResetEmail(String toEmail, String html) throws Exception {
+    private String loadTemplate(String templateName) throws Exception {
+        ClassPathResource resource = new ClassPathResource("templates/email/" + templateName);
+        return StreamUtils.copyToString(resource.getInputStream(), StandardCharsets.UTF_8);
+    }
+
+    private void sendResetEmail(String toEmail, String subject, String html) throws Exception {
         MimeMessage message = mailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(message, false, "UTF-8");
 
         helper.setFrom(fromEmail);
         helper.setTo(toEmail);
-        helper.setSubject("EduNest - Your Password Has Been Reset");
+        helper.setSubject(subject);
         helper.setText(html, true);
 
         mailSender.send(message);
@@ -38,15 +46,11 @@ public class EmailServiceImpl implements EmailService {
     public void sendPasswordResetEmail(String toEmail, String teacherName, String newPassword) {
         try {
             String displayName = (teacherName != null && !teacherName.isBlank()) ? teacherName : "Teacher";
-            String html =
-                    "<p>Dear <b>" + displayName + "</b>,</p>" +
-                    "<p>Your password has been reset as requested.</p>" +
-                    "<p>Your new temporary password is: <b>" + newPassword + "</b></p>" +
-                    "<p>Please log in using this password and change it as soon as possible for your security.</p>" +
-                    "<p>If you did not request this change, please contact your administrator immediately.</p>" +
-                    "<p>Regards,<br>EduNest Team</p>";
+            String html = loadTemplate("sendPasswordResetEmail.html")
+                    .replace("{{displayName}}", displayName)
+                    .replace("{{newPassword}}", newPassword);
 
-            sendResetEmail(toEmail, html);
+            sendResetEmail(toEmail, "EduNest - Your Password Has Been Reset", html);
             log.info("Password reset email sent to {}", toEmail);
         } catch (Exception e) {
             log.error("Failed to send password reset email to {}", toEmail, e);
@@ -57,39 +61,25 @@ public class EmailServiceImpl implements EmailService {
     @Override
     public void sendStudentPasswordResetEmail(String toEmail, List<StudentResetCredential> accounts) {
         try {
-            StringBuilder html = new StringBuilder();
-            html.append("<p>Dear Parent/Student,</p>");
-            html.append("<p>The password for the student account(s) registered with this email address ")
-                    .append("has been reset as requested.</p>");
+            String multiAccountNotice = accounts.size() > 1
+                    ? "<p>This email is linked to <b>" + accounts.size()
+                            + "</b> student accounts. New credentials for each are listed below.</p>"
+                    : "";
 
-            if (accounts.size() > 1) {
-                html.append("<p>This email is linked to <b>").append(accounts.size())
-                        .append("</b> student accounts. New credentials for each are listed below.</p>");
-            }
-
-            html.append("<table cellpadding='8' cellspacing='0' border='1' ")
-                    .append("style='border-collapse:collapse;font-family:Arial,sans-serif;font-size:14px;'>")
-                    .append("<tr style='background-color:#f0f4f8;'>")
-                    .append("<th align='left'>Student</th>")
-                    .append("<th align='left'>Username</th>")
-                    .append("<th align='left'>New Password</th>")
-                    .append("</tr>");
-
+            StringBuilder accountRows = new StringBuilder();
             for (StudentResetCredential account : accounts) {
-                html.append("<tr>")
+                accountRows.append("<tr>")
                         .append("<td>").append(account.getStudentName()).append("</td>")
                         .append("<td><b>").append(account.getUsername()).append("</b></td>")
                         .append("<td><b>").append(account.getNewPassword()).append("</b></td>")
                         .append("</tr>");
             }
 
-            html.append("</table>");
-            html.append("<p>Please log in to the EduNest app using the username and temporary password above, ")
-                    .append("and change your password as soon as possible for your security.</p>");
-            html.append("<p>If you did not request this change, please contact your school immediately.</p>");
-            html.append("<p>Regards,<br>EduNest Team</p>");
+            String html = loadTemplate("sendStudentPasswordResetEmail.html")
+                    .replace("{{multiAccountNotice}}", multiAccountNotice)
+                    .replace("{{accountRows}}", accountRows.toString());
 
-            sendResetEmail(toEmail, html.toString());
+            sendResetEmail(toEmail, "EduNest - Your Password Has Been Reset", html);
             log.info("Student password reset email sent to {} for {} account(s)", toEmail, accounts.size());
         } catch (Exception e) {
             log.error("Failed to send student password reset email to {}", toEmail, e);
