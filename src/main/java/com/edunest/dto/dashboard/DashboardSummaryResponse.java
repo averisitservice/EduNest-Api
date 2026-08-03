f169b787ab695f5a1d04560d3e2e2d0cbd1108b1
patelharshid <averisitservice@gmail.com>
2026-08-03 21:07:29 +0530

    Update DashboardSummaryResponse.java

--- src/main/java/com/edunest/dto/dashboard/DashboardSummaryResponse.java
+++ src/main/java/com/edunest/dto/dashboard/DashboardSummaryResponse.java
@@ -7,6 +7,7 @@ import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
@@ -45,6 +46,6 @@ public class DashboardSummaryResponse {
         private Integer announcementId;
         private String title;
         private String audience;
-        private LocalDate publishDate;
+        private LocalDateTime publishDate;
     }
 }
