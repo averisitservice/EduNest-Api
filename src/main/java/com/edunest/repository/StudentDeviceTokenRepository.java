997343f3ab7fef0de5f150e7a2f273386dd0bc43
patelharshid <averisitservice@gmail.com>
2026-08-10 22:12:16 +0530

    Update StudentDeviceTokenRepository.java

--- src/main/java/com/edunest/repository/StudentDeviceTokenRepository.java
+++ src/main/java/com/edunest/repository/StudentDeviceTokenRepository.java
@@ -2,6 +2,8 @@ package com.edunest.repository;
 
 import com.edunest.entity.StudentDeviceToken;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,7 +14,8 @@ public interface StudentDeviceTokenRepository extends JpaRepository<StudentDevic
 
     Optional<StudentDeviceToken> findByFcmToken(String fcmToken);
 
-    List<String> findDistinctFcmTokenByTenantIdAndStudentIdIn(Integer tenantId, List<Integer> studentIds);
+    @Query("select distinct s.fcmToken from StudentDeviceToken s where s.tenantId = :tenantId and s.studentId in :studentIds")
+    List<String> findDistinctFcmTokenByTenantIdAndStudentIdIn(@Param("tenantId") Integer tenantId, @Param("studentIds") List<Integer> studentIds);
 
     void deleteByFcmToken(String fcmToken);
 
